a348325776c31abcf04d0aedf9d463a62767e95a
a.sivak <riziy_bes@mail.ru>
2021-09-17 13:08:19 +0300

    start

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,5 @@
 'use strict';
 import * as $ from 'jquery';
-import './slick.min';
 
 const mobileWidth = 767;
 let isMobile = checkWidth();
@@ -43,114 +42,6 @@ window.addEventListener('load', function () {
 
     })();
 
-    (function popup() {
-        if (!document.querySelector('.popup')) {
-            return;
-        }
-
-        const popupBtns = [...document.querySelectorAll('.recipes__item')];
-        const popups = [...document.querySelectorAll('.popup')];
-
-        popups.forEach(p => {
-            p.addEventListener('click', hidePopup);
-        });
-
-        popupBtns.forEach(p => {
-            p.addEventListener('click', showPopup);
-        });
-
-        function showPopup() {
-            const id = this.dataset.modal;
-            const popup = document.querySelector(`.popup[data-popup="${id}"]`);
-
-            popup.classList.add('active');
-            document.body.classList.add('no-scrolling');
-        }
-
-        function hidePopup(e) {
-            const target = e.target;
-
-            if (target.dataset.close) {
-                e.preventDefault();
-                const popup = document.querySelector('.popup.active');
-                popup.classList.remove('active');
-                document.body.classList.remove('no-scrolling');
-            }
-        }
-    })();
-
-    (function headerSlider() {
-        if (!document.querySelector('.header__jars')) {
-            return;
-        }
-
-        const sliderSelector = '.header__jars';
-
-        if (isMobile) {
-            initSlider(sliderSelector);
-        }
-
-        window.addEventListener('resize', function (e) {
-            if (isMobile && !document.querySelector(`${sliderSelector}.slick-slider`)) {
-                initSlider(sliderSelector);
-                return;
-            }
-
-            if (!isMobile && document.querySelector('.slick-slider')) {
-                destroySlider(sliderSelector);
-            }
-        });
-
-        function initSlider(slider) {
-            $(slider).slick({
-                infinite: true,
-                slidesToShow: 1,
-                swipeToSlide: true,
-                prevArrow: '<span class="header__jars_arrow lt"></span>',
-                nextArrow: '<span class="header__jars_arrow rt"></span>',
-            })
-        }
-
-        function destroySlider(slider) {
-            $(slider).slick('unslick');
-        }
-    })();
-
-    (function ricepsSlider() {
-        if (!document.querySelector('.recipes')) {
-            return;
-        }
-
-        const sliderSelector = '.recipes';
-
-        if (isMobile) {
-            initSlider(sliderSelector);
-        }
-
-        window.addEventListener('resize', function (e) {
-            if (isMobile && !document.querySelector(`${sliderSelector}.slick-slider`)) {
-                initSlider(sliderSelector);
-                return;
-            }
-
-            if (!isMobile && document.querySelector('.slick-slider')) {
-                destroySlider(sliderSelector);
-            }
-        });
-
-        function initSlider(slider) {
-            $(slider).slick({
-                infinite: true,
-                slidesToShow: 1,
-                prevArrow: '<span class="recipes__arrow lt"></span>',
-                nextArrow: '<span class="recipes__arrow rt"></span>',
-            })
-        }
-
-        function destroySlider(slider) {
-            $(slider).slick('unslick');
-        }
-    })();
 });
 
 function checkWidth() {
